772c7aaa33ec6b66947c200626626d7a04e36a12
Vladimir Kolos <vova.cesar@gmail.com>
2016-04-17 21:37:17 +0300

    Add address autocomplete to website

--- sites/all/modules/features/reiscout_address/reiscout_address.js
+++ sites/all/modules/features/reiscout_address/reiscout_address.js
@@ -1,13 +1,25 @@
 (function ($) {
   Drupal.behaviors.reiscoutTextfieldClearButton = {
     attach: function (context, settings) {
-      $('.clear-address-button[data-input-id]', context).once('clear-button', function (e) {
+      $('.clear-address-button[data-input-id]', context).once('clear-address-button', function () {
         $(this).bind('click', function (e) {
           e.preventDefault();
 
           $('#' + $(this).data('input-id')).val('');
         });
       });
+
+      if (settings.reiscout_address_autocomplete instanceof Array) {
+        for (var i = 0, j = settings.reiscout_address_autocomplete.length; i < j; i++) {
+          $('#' + settings.reiscout_address_autocomplete[i], context).once('address-autocomplete-' + i, function () {
+            var $address = $(this);
+            var placeholder = $address.prop('placeholder');
+            
+            $address.geocomplete();
+            $address.prop('placeholder', placeholder);
+          })
+        }
+      }
     }
   };
 })(jQuery);
