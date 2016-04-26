1b2f57cddfff585f599412bbbca8b7dba2984838
Vladimir Kolos <vova.cesar@gmail.com>
2016-04-26 21:24:34 +0300

    Fix geocomplete bug on the website

--- sites/all/modules/features/reiscout_address/reiscout_address.js
+++ sites/all/modules/features/reiscout_address/reiscout_address.js
@@ -1,25 +1,21 @@
 (function ($) {
   Drupal.behaviors.reiscoutTextfieldClearButton = {
     attach: function (context, settings) {
-      $('.clear-address-button[data-input-id]', context).once('clear-address-button', function () {
+      $('.address-clear-button[data-input-id]', context).once('address-clear-button', function () {
         $(this).bind('click', function (e) {
           e.preventDefault();
-
+          
           $('#' + $(this).data('input-id')).val('');
         });
       });
 
-      if (settings.reiscout_address_autocomplete instanceof Array) {
-        for (var i = 0, j = settings.reiscout_address_autocomplete.length; i < j; i++) {
-          $('#' + settings.reiscout_address_autocomplete[i], context).once('address-autocomplete-' + i, function () {
-            var $address = $(this);
-            var placeholder = $address.prop('placeholder');
-            
-            $address.geocomplete();
-            $address.prop('placeholder', placeholder);
-          })
-        }
-      }
+      $('.address-autocomplete[id]', context).once('address-autocomplete', function () {
+        var $address = $(this);
+        var placeholder = $address.prop('placeholder');
+
+        $address.geocomplete();
+        $address.prop('placeholder', placeholder);
+      });
     }
   };
 })(jQuery);
