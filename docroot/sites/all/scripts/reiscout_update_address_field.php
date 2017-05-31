c80f870c366f7b3eef151cc4437e530c28d3a2e5
Denis Popov <dzutaro@gmail.com>
2017-05-31 13:33:19 +0300

    Create a drush script for automatic updateing the 'Address' field on the all the Property nodes

---
+++ docroot/sites/all/scripts/reiscout_update_address_field.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env drush
+
+function reiscout_update_address_field() {
+  $query = new EntityFieldQuery();
+  $query->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'property');
+  $result = $query->execute();
+
+  if (!isset($result['node'])) {
+    return;
+  }
+
+  $transaction = db_transaction();
+
+  $nids = array_keys($result['node']);
+  foreach ($nids as $nid) {
+    $node = node_load($nid);
+
+    if (!empty($node->field_address[LANGUAGE_NONE][0]) && empty($node->field_address[LANGUAGE_NONE][0]['data'])) {
+      $address = $node->field_address[LANGUAGE_NONE][0];
+
+      $query = array(
+        'address' => $address['thoroughfare'] . ', ' . $address['locality'] . ', ' . $address['administrative_area']. ' ' . $address['postal_code'],
+      );
+      if ($google_map_api_key = variable_get('geocoder_apikey_google')) {
+        $query['key'] = $google_map_api_key;
+      }
+
+      $url = url('https://maps.googleapis.com/maps/api/geocode/json', array('query' => $query));
+      $response = drupal_http_request($url);
+
+      if (200 == $response->code) {
+        $data = json_decode($response->data);
+        if (!empty($data->results[0]->formatted_address)) {
+          $data = array(
+            'formatted_address' => $data->results[0]->formatted_address,
+          );
+          $node->field_address[LANGUAGE_NONE][0]['data'] = serialize($data);
+          node_save($node);
+        }
+      }
+    }
+  }
+}
+
+reiscout_update_address_field();
