36bb4d593918c726b3ce6b5231a947516e185ca0
Vlad Bogatyrov <sorosand@gmail.com>
2017-10-18 13:58:34 +0300

    1st final version for squares dire scanning logic

--- docroot/sites/all/scripts/reiscout_generate_map_square_content.php
+++ docroot/sites/all/scripts/reiscout_generate_map_square_content.php
@@ -2,15 +2,54 @@
 
 <?php
 
-if ($path = drush_get_option('scan-dir')) {
-  drush_print('Scanning dir `' . $path . '` for map squares');
-  if (!is_dir($path)) {
-    drush_print('Directory does not exists.');
+/**
+ * @file
+ * Implements map squares operations.
+ * @see print_description();
+ */
+
+main();
+
+/**
+ * Handle script execution.
+ */
+function main() {
+  $show_help = TRUE;
+
+  if ($path = drush_get_option('scan-dir')) {
+    $show_help = FALSE;
+    drush_print('Scanning dir `' . $path . '` for map squares');
+    if (!is_dir($path)) {
+      drush_print('Directory does not exists.');
+    }
+    $squares = scan_dir_for_squares($path);
+
+    if ($echo_squares = drush_get_option('out-squares')) {
+      drush_print('Scan results:');
+      print_r($squares);
+    }
+
   }
-  $result = scan_dir_for_squares($path);
-  print_r($result);
-} else {
-  print_description();
+
+  if ($show_help) {
+    print_description();
+  }
+}
+
+/**
+ * Print script help text.
+ */
+function print_description () {
+  $help_text = <<<END
+  With that you can:
+  * scan any folder for squares using  --scan-dir=<dir>
+  * output found squares  --out-squares
+  * create nodes for found squares  --create-nodes
+  * output image map for Atlanta  --out-imagemap
+END;
+
+  drush_print($help_text);
+
 }
 
 /**
@@ -55,15 +94,11 @@ function scan_dir_for_squares($path) {
   return $result;
 }
 
-function print_description () {
-  $help_text = <<<END
-  This is the help text
-END;
-
-  drush_print($help_text);
-
-}
-
+/**
+ * @class MapSquareDirectoryIterator
+ *
+ * DirectoryIterator wrapper implementing some help methods for squares directories scanning.
+ */
 class MapSquareDirectoryIterator extends DirectoryIterator {
 
   /**
@@ -82,8 +117,12 @@ class MapSquareDirectoryIterator extends DirectoryIterator {
         }
       }
     }
+    return FALSE;
   }
 
+  /**
+   * Detect is a current dir is a square dir.
+   */
   public function isSquareDir() {
     $extentions = array('gpx', 'kmz');
     foreach ($this as $item) {
