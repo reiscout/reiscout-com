ec849f6cc878cf1e78888440918faff0a130ce7e
Vlad Bogatyrov <sorosand@gmail.com>
2017-10-18 15:03:15 +0300

    use namespaces

--- docroot/sites/all/scripts/reiscout_generate_map_square_content.php
+++ docroot/sites/all/scripts/reiscout_generate_map_square_content.php
@@ -8,39 +8,92 @@
  * @see print_description();
  */
 
-main();
+namespace reiscout_generate_map_square_content {
 
-/**
- * Handle script execution.
- */
-function main() {
-  $show_help = TRUE;
+  use DirectoryIterator;
 
-  if ($path = drush_get_option('scan-dir')) {
-    $show_help = FALSE;
-    drush_print('Scanning dir `' . $path . '` for map squares');
-    if (!is_dir($path)) {
-      drush_print('Directory does not exists.');
+  /**
+   * @class MapSquareDirectoryIterator
+   *
+   * DirectoryIterator wrapper implementing some help methods for squares directories scanning.
+   */
+  class MapSquareDirectoryIterator extends DirectoryIterator {
+
+    /**
+     * Return path for file by extention in directory.
+     *
+     * @param string $ext extention
+     *
+     * @return SplFileInfo
+     */
+    public function getFileByExt ($ext) {
+      foreach ($this as $item) {
+        if (!$item->isDot() && $item->isFile()) {
+          $file = $item->getFileInfo();
+          if ($file->getExtension() == $ext) {
+            return $file;
+          }
+        }
+      }
+      return FALSE;
     }
-    $squares = scan_dir_for_squares($path);
 
-    if ($echo_squares = drush_get_option('out-squares')) {
-      drush_print('Scan results:');
-      print_r($squares);
+    /**
+     * Detect is a current dir is a square dir.
+     */
+    public function isSquareDir() {
+      $extentions = array('gpx', 'kmz');
+      foreach ($this as $item) {
+        if (!$item->isDot() && $item->isFile() && in_array($item->getFileInfo()->getExtension(), $extentions)) {
+          return TRUE;
+        }
+      }
+      return FALSE;
     }
 
-  }
-
-  if ($show_help) {
-    print_description();
   }
 }
 
-/**
- * Print script help text.
- */
-function print_description () {
-  $help_text = <<<END
+namespace reiscout_generate_map_square_content {
+  
+  main();
+
+  /**
+   * Handle script execution.
+   */
+  function main() {
+    $show_help = TRUE;
+
+    if ($path = drush_get_option('scan-dir')) {
+      $show_help = FALSE;
+      drush_print('Scanning dir `' . $path . '` for map squares');
+      if (!is_dir($path)) {
+        drush_print('Directory does not exists.');
+      }
+      $squares = scan_dir_for_squares($path);
+
+      if ($echo_squares = drush_get_option('out-squares')) {
+        drush_print('Scan results:');
+        print_r($squares);
+      }
+
+      if ($echo_squares = drush_get_option('create-nodes')) {
+        drush_print('Creating nodes for '. count($squares). 'found squares');
+        create_nodes($squares);
+      }
+
+    }
+
+    if ($show_help) {
+      print_description();
+    }
+  }
+
+  /**
+   * Print script help text.
+   */
+  function print_description () {
+    $help_text = <<<END
   With that you can:
   * scan any folder for squares using  --scan-dir=<dir>
   * output found squares  --out-squares
@@ -48,85 +101,60 @@ function print_description () {
   * output image map for Atlanta  --out-imagemap
 END;
 
-  drush_print($help_text);
+    drush_print($help_text);
 
-}
-
-/**
- * Recursively scan passed directory and return all found squares as a flat (not nested) array.
- * Result format:
- *   array(
- *     'N3996335E_9401244' => array(
- *       'gpx'  => 'file path',
- *       'kmz' => 'file path',
- *        ...
- *       'png' => 'file path',
- *     ),
- *     1 => ...
- *   )
- *
- * @return array
- */
-function scan_dir_for_squares($path) {
-
-  $directoryIterator = new MapSquareDirectoryIterator($path);
-
-  $result = array();
-
-  $detect_files = array('png', 'gpx', 'kmz', 'kml');
-  foreach ($detect_files as $ext) {
-    if ($file = $directoryIterator->getFileByExt($ext)) {
-      $result[$file->getBasename('.'.$file->getExtension())][$ext] = $directoryIterator->getFileByExt($ext);
-    }
-  }
-
-  foreach($directoryIterator as $item) {
-    if (!$item->isDot() && $item->isDir()) {
-      $result = array_merge_recursive($result, scan_dir_for_squares($item->getRealPath()));
-    }
-
-  }
-
-  return $result;
-}
-
-/**
- * @class MapSquareDirectoryIterator
- *
- * DirectoryIterator wrapper implementing some help methods for squares directories scanning.
- */
-class MapSquareDirectoryIterator extends DirectoryIterator {
-
-  /**
-   * Return path for file by extention in directory.
-   *
-   * @param string $ext extention
-   *
-   * @return SplFileInfo
-   */
-  public function getFileByExt ($ext) {
-    foreach ($this as $item) {
-      if (!$item->isDot() && $item->isFile()) {
-        $file = $item->getFileInfo();
-        if ($file->getExtension() == $ext) {
-          return $file;
-        }
-      }
-    }
-    return FALSE;
   }
 
   /**
-   * Detect is a current dir is a square dir.
+   * Recursively scan passed directory and return all found squares as a flat (not nested) array.
+   * Result format:
+   *   array(
+   *     'N3996335E_9401244' => array(
+   *       'gpx'  => 'file path',
+   *       'kmz' => 'file path',
+   *        ...
+   *       'png' => 'file path',
+   *     ),
+   *     1 => ...
+   *   )
+   *
+   * @return array
    */
-  public function isSquareDir() {
-    $extentions = array('gpx', 'kmz');
-    foreach ($this as $item) {
-      if (!$item->isDot() && $item->isFile() && in_array($item->getFileInfo()->getExtension(), $extentions)) {
-        return TRUE;
+  function scan_dir_for_squares($path) {
+
+    $directoryIterator = new MapSquareDirectoryIterator($path);
+
+    $result = array();
+
+    $detect_files = array('png', 'gpx', 'kmz', 'kml');
+    foreach ($detect_files as $ext) {
+      if ($file = $directoryIterator->getFileByExt($ext)) {
+        $result[$file->getBasename('.'.$file->getExtension())][$ext] = $directoryIterator->getFileByExt($ext);
       }
     }
-    return FALSE;
+
+    foreach($directoryIterator as $item) {
+      if (!$item->isDot() && $item->isDir()) {
+        $result = array_merge_recursive($result, scan_dir_for_squares($item->getRealPath()));
+      }
+
+    }
+
+    return $result;
   }
 
+  /**
+   * Create nodes for passed squares
+   *
+   * @param array $squares
+   *
+   * @return integer
+   *   Number of created nodes.
+   */
+  function create_nodes ($squares) {
+
+  }
+
+
 }
+
